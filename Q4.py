787b50a40edba208b8f8fe94765a5d305a6ba7be
enking <engur9902@gmail.com>
2024-06-21 00:22:05 +0900

    45분 전으로 타이머 돌리기

--- Q4.py
+++ Q4.py
@@ -6,6 +6,7 @@ import sys
 
 First_Wake_H,First_Wake_M= map(int,sys.stdin.readline().split())# 최초 기상 시간을 입력을 받음 
 More_Sleep =  int(sys.stdin.readline()) # 추가로 더 자고 싶은 시간을 입력
+
 More_Sleep_H = int(More_Sleep / 60) #  추가로 자고 싶은 시간의 시간
 More_Sleep_M = int(More_Sleep % 60) # 추가로 자고 싶은 시간의 분
 
