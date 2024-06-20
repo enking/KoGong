b00f5c5da7942099e21990a9124d0c09be7111de
enking <engur9902@gmail.com>
2024-06-20 17:29:19 +0900

    판다 먹이 비례 계산 첨삭본 추가

--- Q3.py
+++ Q3.py
@@ -20,3 +20,18 @@ a2 = sum(result)
 
 print(" ".join(map(str, result)))
 print(a2)
+####################################################################
+#참고 링크 https://velog.io/@yeseolee/Python-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%A0%A5-%EC%A0%95%EB%A6%ACsys.stdin.readline
+# 풀이 2 첨삭 결과
+import sys
+
+n = int(sys.stdin.readline())  # int(input()) 보다 빠름
+s = 0
+
+for i in range(n):
+    a, b, c = map(int, sys.stdin.readline().split())
+    result = int((c / a) * b)
+    print(result)
+    s += result
+
+print(s)
