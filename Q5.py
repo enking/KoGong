7ef1ec48a95a44bbe23e293fe880bc9106082876
enking <engur9902@gmail.com>
2024-07-08 15:17:56 +0900

    240708 깃 수업간 특이사항 업로드

--- Q5.py
+++ Q5.py
@@ -2,6 +2,7 @@
 # 시간을 빼는 문제 아까 판다는 더하고 이건 빼는것
 # 이 문제의 포인트는 45분을 빼는데 분이 45분보다 작으면 1시간을 빼서 분에 15분을 더해주는 것
 
+
 import sys
 
 firstButtonPush_H, firstButtonPush_M = map(int,sys.stdin.readline().split())# 최초 시간을 입력을 받음 
